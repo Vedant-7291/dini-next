91d57b179fafc7a4a022e0fab7111447892ea42f
Vedant-7291 <vedant115619@gmail.com>
2025-08-26 23:18:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Any request that starts with /api/
+        destination: 'http://localhost:5000/api/:path*', // Gets forwarded to Express
+      },
+    ];
+  },
+};
 
 export default nextConfig;
