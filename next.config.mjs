a60c9edeb0c68eea12af086b8c0668ec089092a3
Vedant-7291 <vedant115619@gmail.com>
2025-08-27 17:02:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://dini-next-3.onrender.com'
+        destination: 'https://dini-next-4.onrender.com'
       },
     ];
   },
